6d15efd20ebf1318d0e59ee6b469107a07bd863a
logofic <65857886+logofic@users.noreply.github.com>
2020-06-03 21:33:05 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import "./Header.scss";
 export default function Header() {
   return (
     <header>
-      <h1>CodeRigade</h1>
+      <h1>CodeColab</h1>
       <h5>Real-time collaborative code editor</h5>
     </header>
   );
